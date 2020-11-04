87aa319ad0763aa8f10b80e3d08cfd27168f2c36
Gnutt Halvordsson <gnutt@halvordsson.se>
2020-11-04 07:58:33 +0100

    Updated help comments, and changed default image size

--- New-Gravatar.ps1
+++ New-Gravatar.ps1
@@ -1,10 +1,31 @@
+<#
+.DESCRIPTION
+Generate and download a Gravatar for a EmailAddress.
 
+.PARAMETER EmailAddress
+The emailaddress for which to fetch a gravatar. Does not have to be a email address. String will be trimmed, and lowercased. After which a md5 hash will be generated. The MD5 is what really is used for the download.
+
+.PARAMETER ForceImage
+Always download a gravatar image, even if user has pushed "personal" image to the gravatar system.
+
+.EXAMPLE
+./New-Gravatar -EmailAddress joe@example.com
+Download the gravatar for joe@example.com
+
+.EXAMPLE
+./New-Gravatar -EmailAddress joe@example.com -GravatarType robohash -ForceImage
+Download a Robot Gravatar for joe@example.com, even if they have a personal gravatar image.
+
+.LINK
+https://en.gravatar.com/site/implement/images/
+
+#>
 [CmdletBinding()]
 param (
     [Parameter()][String]$EmailAddress,
     [Parameter()][Switch]$ForceImage,
     [Parameter()][ValidateSet("identicon", "monsterid", "wavatar", "retro", "robohash")][String]$GravatarType = "monsterid",
-    [Parameter()][Int]$ImageSize = 200
+    [Parameter()][Int]$ImageSize = 400
 )
 
 $EmailAddress = $EmailAddress.Trim().toLower()
